f56440c14dc70c1ef94dd34a279953d7fb8d153d
nayukidayo <kurumi@nayuki.moe>
2021-12-10 12:14:14 +0800

    fix

--- src/db.js
+++ src/db.js
@@ -1,18 +1,21 @@
 const fastifyPlugin = require('fastify-plugin')
-const mysql = require('mysql2/promise')
+const mysql = require('mysql2')
 
 async function dbConnector(fastify, options) {
-  const connection = await mysql.createConnection({
+  const pool = await mysql.createPool({
     host: '152.136.200.17',
     user: 'root',
     password: 'nayukidayo',
     database: 'bladex',
     charset: 'utf8mb4',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
   })
-  fastify.decorate('db', connection)
+  const db = pool.promise()
+  fastify.decorate('db', db)
 }
 
 module.exports = fastifyPlugin(dbConnector)
 
-
-// docker run --name mysql-avue -p 3306:3306 -v /srv/avue/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=nayukidayo -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci --max_allowed_packet=32505856
+// docker run --restart=always --name mysql-avue -p 3306:3306 -v /srv/avue/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=nayukidayo -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci --max_allowed_packet=32505856
